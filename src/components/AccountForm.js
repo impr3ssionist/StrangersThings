0bedb96e31d8a1c12fef933ce4f37cc4c9610ca3
Caleb <cpford13@gmail.com>
2021-12-04 19:47:16 -0600

    Refactored according the second lecure AccountForm and src/index.js

--- src/components/AccountForm.js
+++ src/components/AccountForm.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
+import { callApi } from "../api";
 
 const API_ROOT = `https://strangers-things.herokuapp.com/api/2108-LSU-RM-WEB-PT/users/`;
 const API_REGISTER = `${API_ROOT}register`;
@@ -12,48 +13,25 @@ const AccountForm = ({ action, setToken, setUserData }) => {
   const isLogin = action === "login";
   const title = isLogin ? "Login" : "Register";
   const oppositeTitle = isLogin ? "Register" : "Login";
-  const oppositeAction = isLogin ? "Register" : "Login";
+  const oppositeAction = isLogin ? "register" : "login";
   const actionURL = isLogin ? API_LOGIN : API_REGISTER;
   const history = useHistory();
 
   const handleSubmit = async (event) => {
     event.preventDefault();
-    console.log(`Username: ${username}`);
-    console.log(`Password: ${password}`);
-    try {
-      const response = await fetch(API_REGISTER, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          user: {
-            username: username,
-            password: password,
-          },
-        }),
-      });
-
-      const { data } = await response.json();
-      const token = data.token;
-      const message = data.message;
-      console.log(`Token: ${token}`);
-      console.log(`Message: ${message}`);
-      if (token) {
-        setToken(token);
-        const userResponse = await fetch(API_USER, {
-          headers: {
-            "Content-type": "application/json",
-            Authorization: `Bearer ${token}`,
-          },
-        });
-        const { data } = await userResponse.json();
-        console.log(data);
-        setUserData(data);
-        history.push("/");
-      }
-    } catch (error) {
-      console.error(`Error registering a User ${error}`);
+    const data = await callApi({
+      url: `/users/${action}`,
+      body: { user: { username, password } },
+      method: "POST",
+    });
+    const token = data?.data?.token;
+    console.log("token in account", token);
+    if (token) {
+      localStorage.setItem("token", token);
+      setUsername("");
+      setPassword("");
+      setToken(token);
+      history.push("/");
     }
   };
   return (
--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { AccountForm } from "./components";
+import { callApi } from "./api";
 
 const App = () => {
   const [token, setToken] = useState("");
   const [userData, setUserData] = useState({});
+
+  const fetchUserData = async (token) => {
+    const { data } = await callApi({
+      url: "/users/me",
+      token,
+    });
+    return data;
+  };
+
+  useEffect(async () => {
+    if (!token) {
+      localStorage.getItem("token");
+      return;
+    }
+    const data = await fetchUserData(token);
+    if (data && data.username) {
+      setUserData(data);
+    }
+  }, [token]);
+
   return (
     <>
       <h1>Stranger's Things</h1>
+      {userData.username && <div>Hello {userData.username}</div>}
 
       <Route path="/register">
         <AccountForm
